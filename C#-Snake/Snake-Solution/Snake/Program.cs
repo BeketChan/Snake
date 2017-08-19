102b628b035a089441ad9d5cd83b8493fdcbf14e
BeketChan <roman260479@yandex.ru>
2017-08-19 12:55:13 +0300

    Осваиваем List

--- C#-Snake/Snake-Solution/Snake/Program.cs
+++ C#-Snake/Snake-Solution/Snake/Program.cs
@@ -10,53 +10,27 @@ namespace Snake
   {
     static void Main(string[] args)
     {
-      /*int x = 1;
-      Func1(x);
-      Console.WriteLine("Func1(x) = " + x);
+      Point p1 = new Point(1, 1, '*');
+      Point p2 = new Point(1, 2, '*');
+      Point p3 = new Point(1, 2, '*');
+      Point p4 = new Point(2, 2, '*');
+      Point p5 = new Point(3, 2, '*');
+      Point p6 = new Point(3, 3, '*');
 
-      x = 1;
-      Func2(x);
-      Console.WriteLine("Func2(x) = " + x);
+      List<Point> pointsList = new List<Point>();
+      pointsList.Add(p1);
+      pointsList.Add(p2);
+      pointsList.Add(p3);
+      pointsList.Add(p4);
+      pointsList.Add(p5);
+      pointsList.Add(p6);
 
-      x = 1;
-      Func3(x);
-      Console.WriteLine("Func3(x) = " + x);*/
-
-      Point p1 = new Point(1, 3, '*');
-      p1.Drow();
-      Move(p1, 10, 10);
-      p1.Drow();
-
-      Point p2 = new Point(4, 5, '#');
-      p2.Drow();
-      Reset(p2);
-      p2.Drow();
+      foreach(Point p in pointsList)
+      {
+        p.Drow();
+      }
 
       Console.ReadKey();
     }
-
-    static void Func1(int value){}
-
-    static void Func2(int value)
-    {
-      value++;
-    }
-
-    static void Func3(int x)
-    {
-      x++;
-    }
-
-    static void Move(Point p, int dx, int dy)
-    {
-      p.x += dx;
-      p.y += dy;
-    }
-
-    static void Reset(Point p)
-    {
-      p = new Point();
-    }
-
   }
 }
