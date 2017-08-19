0d7a984f64ad0e0f229a85d66f1abaf517351d64
BeketChan <roman260479@yandex.ru>
2017-08-19 12:33:27 +0300

    Поэксперементировали с RAM

--- C#-Snake/Snake-Solution/Snake/Program.cs
+++ C#-Snake/Snake-Solution/Snake/Program.cs
@@ -10,13 +10,53 @@ namespace Snake
   {
     static void Main(string[] args)
     {
-      Point p1 = new Point(1,3,'*');
+      /*int x = 1;
+      Func1(x);
+      Console.WriteLine("Func1(x) = " + x);
+
+      x = 1;
+      Func2(x);
+      Console.WriteLine("Func2(x) = " + x);
+
+      x = 1;
+      Func3(x);
+      Console.WriteLine("Func3(x) = " + x);*/
+
+      Point p1 = new Point(1, 3, '*');
+      p1.Drow();
+      Move(p1, 10, 10);
       p1.Drow();
 
-      Point p2 = new Point(4,5,'#');
+      Point p2 = new Point(4, 5, '#');
+      p2.Drow();
+      Reset(p2);
       p2.Drow();
 
       Console.ReadKey();
     }
+
+    static void Func1(int value){}
+
+    static void Func2(int value)
+    {
+      value++;
+    }
+
+    static void Func3(int x)
+    {
+      x++;
+    }
+
+    static void Move(Point p, int dx, int dy)
+    {
+      p.x += dx;
+      p.y += dy;
+    }
+
+    static void Reset(Point p)
+    {
+      p = new Point();
+    }
+
   }
 }
